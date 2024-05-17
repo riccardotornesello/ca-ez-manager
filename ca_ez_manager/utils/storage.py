7c902caa5ee5d50686db1ec2f8e608889b58bfd1
riccardo.tornesello <riccardo.tornesello@gmail.com>
2024-05-17 21:33:08 +0200

    refactor: avoid duplicated code in storage utils

--- ca_ez_manager/utils/storage.py
+++ ca_ez_manager/utils/storage.py
@@ -12,56 +12,63 @@ from ca_ez_manager.utils.crypto import (
 
 # TODO: allow custom directory
 user_home = os.path.expanduser("~")
-ca_folder = os.path.join(user_home, ".ca")
+ca_root_folder = os.path.join(user_home, ".ca")
 
 NECESSARY_CA_FILES = ["ca.key", "ca.pem"]
 
 
 def init_storage():
-    if not os.path.exists(ca_folder):
-        os.makedirs(ca_folder)
+    if not os.path.exists(ca_root_folder):
+        os.makedirs(ca_root_folder)
 
 
 def get_ca_list() -> List[str]:
     init_storage()
 
     # Get the list of subdirectories in the CA folder
-    subdirs = os.listdir(ca_folder)
+    subdirs = os.listdir(ca_root_folder)
 
     # Filter out only the directories
-    subdirs = [d for d in subdirs if os.path.isdir(f"{ca_folder}/{d}")]
+    subdirs = [d for d in subdirs if os.path.isdir(f"{ca_root_folder}/{d}")]
 
     # Return only the directories that contain the necessary files
-    return [d for d in subdirs if all(os.path.exists(f"{ca_folder}/{d}/{fn}") for fn in NECESSARY_CA_FILES)]
+    return [d for d in subdirs if all(os.path.exists(f"{ca_root_folder}/{d}/{fn}") for fn in NECESSARY_CA_FILES)]
 
 
 def store_ca(ca_name: str, private_key, cert):
-    if os.path.exists(f"{ca_folder}/{ca_name}"):
+    selected_ca_folder = f"{ca_root_folder}/{ca_name}"
+
+    if os.path.exists(selected_ca_folder):
         raise FileExistsError(f"CA {ca_name} already exists")
 
-    os.makedirs(f"{ca_folder}/{ca_name}")
+    os.makedirs(selected_ca_folder)
 
-    save_private_key(private_key, f"{ca_folder}/{ca_name}/ca.key")
-    save_certificate(cert, f"{ca_folder}/{ca_name}/ca.pem")
+    save_private_key(private_key, f"{ca_root_folder}/{ca_name}/ca.key")
+    save_certificate(cert, f"{ca_root_folder}/{ca_name}/ca.pem")
 
 
 def get_ca(ca_name: str):
+    selected_ca_folder = f"{ca_root_folder}/{ca_name}"
+
     for fn in NECESSARY_CA_FILES:
-        if not os.path.exists(f"{ca_folder}/{ca_name}/{fn}"):
+        if not os.path.exists(f"{selected_ca_folder}/{fn}"):
             raise FileNotFoundError(f"CA {ca_name} not found")
 
-    private_key = load_private_key(f"{ca_folder}/{ca_name}/ca.key")
-    cert = load_certificate(f"{ca_folder}/{ca_name}/ca.pem")
+    private_key = load_private_key(f"{selected_ca_folder}/ca.key")
+    cert = load_certificate(f"{selected_ca_folder}/ca.pem")
 
     return private_key, cert
 
 
 def store_cert(ca_name: str, cert_name: str, private_key, cert, csr):
-    if os.path.exists(f"{ca_folder}/{ca_name}/{cert_name}"):
+    selected_ca_folder = f"{ca_root_folder}/{ca_name}"
+    cert_folder = f"{selected_ca_folder}/{cert_name}"
+
+    if os.path.exists(cert_folder):
         raise FileExistsError(f"Certificate {cert_name} already exists")
 
-    os.makedirs(f"{ca_folder}/{ca_name}/{cert_name}")
+    os.makedirs(cert_folder)
 
-    save_private_key(private_key, f"{ca_folder}/{ca_name}/{cert_name}/{cert_name}.key")
-    save_certificate(cert, f"{ca_folder}/{ca_name}/{cert_name}/{cert_name}.pem")
-    save_csr(csr, f"{ca_folder}/{ca_name}/{cert_name}/{cert_name}.csr")
+    save_private_key(private_key, f"{cert_folder}/{cert_name}.key")
+    save_certificate(cert, f"{cert_folder}/{cert_name}.pem")
+    save_csr(csr, f"{cert_folder}/{cert_name}.csr")
